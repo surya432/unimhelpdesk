c7e1212740d71740bab806cc4d725f17cf4f2983
surya432 <hadisurya432>
2019-07-11 11:41:58 +0700

    modif23

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::post('deploy', 'DeployController@deploy');
-Route::get('deploy', 'DeployController@deploy');
 
 Route::group(['middleware' => [ 'web', 'auth']], function () {
     Route::get('/admin', 'HomeController@index')->name('home');
