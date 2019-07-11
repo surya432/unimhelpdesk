1b19b2977cfab96f440d6bb11d1bbdfa990a8388
surya432 <hadisurya432>
2019-07-11 09:12:27 +0700

    remove gak penting@

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 Route::post('login', 'Api\LoginController@login');
 
+
 Route::group(['middleware' => [ 'auth:api']], function () {
     Route::post( '/master', 'Api\TiketController@getMaster')->name( 'getMaster');
 
