57deadaad5e9457539a752624d4a0b1064bb01a3
surya432 <hadisurya432>
2019-07-19 14:27:42 +0700

    errorsad

--- resources/views/admin/tiket/show.blade.php
+++ resources/views/admin/tiket/show.blade.php
@@ -97,7 +97,11 @@
         </div>
         {!! Form::open(array('route' => 'tiket.replyTiket','method'=>'POST','files'=> true)) !!}
         {{Form::hidden('senders', Auth::user()->name, array('id' => 'senders')) }}
-        
+        @if (Auth::user()->hasRole("User"))
+            {{Form::hidden('repply', 0, array('id' => 'repply')) }}
+        @else
+            {{Form::hidden('repply', 1, array('id' => 'repply')) }}
+        @enfif
         {{Form::hidden('tiket_id', $tiket->id, array('id' => 'tiket_id')) }}
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="panel panel-primary">
