4f2a0173ec1f01d5d5f23234506a6d876c43056f
surya432 <hadisurya432>
2019-07-20 11:20:12 +0700

    modif tiket api

--- app/Http/Controllers/Api/TiketController.php
+++ app/Http/Controllers/Api/TiketController.php
@@ -43,10 +43,10 @@ class TiketController extends Controller
     {
         if ($request->user()->hasRole('User')) {
             $data = \App\Tiket::join('users', 'tikets.user_id', 'users.id')
-                ->join('content_tikets', 'content_tikets.id', 'tikets.id')
-                ->join('departements', 'departements.id', 'tikets.departement_id')
-                ->join('statuses', 'statuses.id', 'tikets.status_id')
-                ->join('prioritas', 'prioritas.id', 'tikets.status_id')
+                //->join('content_tikets', 'content_tikets.id', 'tikets.id')
+                //->join('departements', 'departements.id', 'tikets.departement_id')
+                //->join('statuses', 'statuses.id', 'tikets.status_id')
+                //->join('prioritas', 'prioritas.id', 'tikets.status_id')
                 ->where('tikets.user_id', $request->user()->id)
                 ->select('tikets.*', 'users.name as userName', 'prioritas.name as prioritasName', 'departements.name as departementName', 'statuses.name as statusName')
                 ->orderBy('tikets.updated_at', 'DESC')->get();
